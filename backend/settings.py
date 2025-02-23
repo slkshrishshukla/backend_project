22e7a9c234a0bae23a5ca8b544255e1604295fc8
slkshrishshukla <slk.shrishshukla@gmail.com>
2025-02-23 23:35:35 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-v+w86s_wnbbes&2livp0oqmyvo#vx7jw#)g&sa-!jol^ds)o6s'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG" "False").lower() == True 
+DEBUG = os.environ.get("DEBUG" "False").lower() == 'True' 
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
